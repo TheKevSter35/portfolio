4df82e3cdd902e792457632cd7dd0c764f9705f8
TheKevSter35 <43183768+TheKevSter35@users.noreply.github.com>
2021-09-14 16:42:51 +0200

    added work id page + styling of it

--- src/components/pages/portfolioWork.jsx
+++ src/components/pages/portfolioWork.jsx
@@ -1,43 +1,70 @@
-import React, {useEffect} from 'react'
-import { setProject, }  from '../../actions/global-actions'
-import { connect } from 'react-redux'
-import { useParams } from 'react-router'
+import React, {Fragment, useEffect} from 'react'
+import {setProject} from '../../actions/global-actions'
+import {connect} from 'react-redux'
+import {useParams} from 'react-router'
 
-
-const PortfolioWork = ({ project, setProject }) => {
- const param = useParams()
+const PortfolioWork = ({project, setProject}) => {
+    const param = useParams()
     useEffect(() => {
         setProject(param.id)
-    },[])
-
+    }, [])
 
     return (
         <main>
-            <section className="intro">
-                <div className="intro__inner inner">
-         <ul>
-                
-                <h2>Mijn porjec </h2>
-                <p>{project.name}</p>
-      
-       </ul>
-       </div>
+            <section className="banner">
+                <div className="banner__inner inner">
+                    <img src={project.banner} alt={project.name}/>
+
+                </div>
             </section>
+
+            <section className="copy">
+                <div className="copy__inner inner">
+                    <div className="copy">
+                        <h1>{project.name}</h1>
+                        <p>{project.description}</p>
+                    </div>
+                    <div className="image">
+                        <img src={project.image} alt={project.name}/>
+                    </div>
+                </div>
+            </section>
+
+            {project.demolink && 
+            <section className="demolink">
+                <div className="demolink__inner inner" style={{
+                                background: `${project.bgcolor}`
+                            }}>
+                    <h2>Links</h2>
+                    <a
+                        className="primary-button"
+                        href={`${project.demolink}`}
+                        target="_blank"
+                        rel="noreferrer">Demo
+                    </a>
+                    {project.repository && 
+                    <Fragment>
+                        <a
+                        className="primary-button"
+                        href={`${project.repository}`}
+                        target="_blank"
+                        rel="noreferrer">Repository
+                    </a>
+                    </Fragment>
+                    }
+
+                </div>
+            </section>
+}
+
         </main>
     )
 }
 
-
-const mapStateToProps = state => ({
-	project: state.global.project
-})
+const mapStateToProps = state => ({project: state.global.project})
 
 const mapDispatchToProps = dispatch => ({
     setProject: id => dispatch(setProject(id))
 })
 
-
-
-
 export default connect(mapStateToProps, mapDispatchToProps)(PortfolioWork)
-
