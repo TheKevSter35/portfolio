c7dfb1b879b68ebecf55598c61acd4bf932afff4
TheKevSter35 <43183768+TheKevSter35@users.noreply.github.com>
2021-10-29 22:46:24 +0200

    cleaning

--- src/components/functions/darkmodebutton.jsx
+++ src/components/functions/darkmodebutton.jsx
@@ -3,18 +3,12 @@ import {React, useEffect} from 'react'
 const DarkMode = () => {
 
     useEffect(() => {
-           
-
-if(window.matchMedia('(prefers-color-scheme: dark)').matches){
-	// console.log('Currently in dark mode');
-    const element = document.querySelector('body');
+        if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            const element = document.querySelector('body');
             element
                 .classList
                 .add("dark")
-}
-// else{
-// 	console.log('Currently not in dark mode');
-// }
+        }
     }, [])
 
     const handleOnClick = () => {
