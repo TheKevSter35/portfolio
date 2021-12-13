9f1856ed21db3353201c980956e4b66db84326a6
TheKevSter35 <43183768+TheKevSter35@users.noreply.github.com>
2021-12-13 16:46:06 +0100

    testing heroku api with kaffeine pings

--- src/actions/global-actions.js
+++ src/actions/global-actions.js
@@ -1,7 +1,6 @@
 //thunk API CALL
 export const getItems = () => async dispatch =>{
-	const payload = await fetch(`https://my-json-server.typicode.com/TheKevSter35/portfolio-db/portfolio`)
-	// const payload = await fetch(`http://localhost:3001/portfolio/`)
+	const payload = await fetch(`https://kevincuriapi.herokuapp.com/portfolio`)
 	const response = await payload.json()
 	dispatch(updateItems(response))
 }
@@ -13,8 +12,7 @@ export const updateItems = value => ({
 
 
 export const setProject = (id) => async dispatch =>{
-	const payload = await fetch(`https://my-json-server.typicode.com/TheKevSter35/portfolio-db/portfolio/${id}`)
-	// const payload = await fetch(`http://localhost:3001/portfolio/${id}`)
+	const payload = await fetch(`https://kevincuriapi.herokuapp.com/portfolio/${id}`)
 	const response = await payload.json()
 	if(payload.status === 404){
 		dispatch(errorProject(true))
