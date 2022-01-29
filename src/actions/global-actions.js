683350f92b786878c01f0cd0d0cfb2768834cd14
TheKevSter35 <43183768+TheKevSter35@users.noreply.github.com>
2022-01-29 11:52:19 +0100

    change api

--- src/actions/global-actions.js
+++ src/actions/global-actions.js
@@ -1,18 +1,16 @@
 //thunk API CALL
 export const getItems = () => async dispatch =>{
-	const payload = await fetch(`https://kevincuriapi.herokuapp.com/portfolio`)
+	// const payload = await fetch(`https://kevincuriapi.herokuapp.com/portfolio`)
+	const payload = await fetch(`https://my-json-server.typicode.com/TheKevSter35/portfolio-db/portfolio`)
 	const response = await payload.json()
 	dispatch(updateItems(response))
 }
-// action
-export const updateItems = value => ({
-	type: 'UPDATE_ITEMS',
-	value
-})
+
 
 
 export const setProject = (id) => async dispatch =>{
-	const payload = await fetch(`https://kevincuriapi.herokuapp.com/portfolio/${id}`)
+	// const payload = await fetch(`https://kevincuriapi.herokuapp.com/portfolio/${id}`)
+	const payload = await fetch(`https://my-json-server.typicode.com/TheKevSter35/portfolio-db/portfolio/${id}`)
 	const response = await payload.json()
 	if(payload.status === 404){
 		dispatch(errorProject(true))
@@ -21,7 +19,13 @@ export const setProject = (id) => async dispatch =>{
 	dispatch(updateProject(response))	
 	}
 }
+
 // action
+export const updateItems = value => ({
+	type: 'UPDATE_ITEMS',
+	value
+})
+
 export const updateProject = value => ({
 	type: 'UPDATE_PROJECT',
 	value
