8acccc65de72b48ac0dacab018d5af20d9fbb72f
TheKevSter35 <43183768+TheKevSter35@users.noreply.github.com>
2022-08-29 16:16:31 +0200

    add aria label

--- src/components/functions/darkmodebutton.jsx
+++ src/components/functions/darkmodebutton.jsx
@@ -1,32 +1,29 @@
-import {React, useEffect} from 'react'
+import { React, useEffect } from "react";
 
 const DarkMode = () => {
-
-    useEffect(() => {
-        if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-            const element = document.querySelector('body');
-            element
-                .classList
-                .add("dark")
-        }
-    }, [])
-
-    const handleOnClick = () => {
-        const element = document.querySelector('body');
-        element
-            .classList
-            .toggle("dark")
+  useEffect(() => {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      const element = document.querySelector("body");
+      element.classList.add("dark");
     }
+  }, []);
 
-    return (
-        <button
-            onClick={() => {
-            handleOnClick()
-        }}
-            className='darkmodebutton'>
-            <span></span>
-        </button>
-    )
-}
+  const handleOnClick = () => {
+    const element = document.querySelector("body");
+    element.classList.toggle("dark");
+  };
 
-export default DarkMode
+  return (
+    <button
+      onClick={() => {
+        handleOnClick();
+      }}
+      aria-label="theme button"
+      className="darkmodebutton"
+    >
+      <span></span>
+    </button>
+  );
+};
+
+export default DarkMode;
